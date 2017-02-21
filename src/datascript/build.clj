806ac79f82430750ce2785d7da02ade0e622f6ac
Nikita Prokopov <prokopov@gmail.com>
2017-02-21 13:19:32 +0300

    no-parallel option

--- src/datascript/build.clj
+++ src/datascript/build.clj
@@ -2,15 +2,11 @@
   (:require
     [cljs.build.api :as b]))
 
-
 (defn -main [& args]
   (b/build 
-    (b/inputs "src")
+    "src"
     { :output-to      "target/datascript.js"
       :output-dir     "target"
       :optimizations  :advanced
-      :pretty-print   true
-      :parallel-build true
-      :elide-asserts true
-      :output-wrapper false }))
+      :parallel-build (not= "--no-parallel" (first args)) }))
  
